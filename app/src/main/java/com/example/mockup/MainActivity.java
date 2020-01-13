c79a0f4aec6b7215f6fb988c2d2c612329b3f282
pollyana-eng <pollckmg@gmail.com>
2020-01-13 11:43:10 -0300

    Atualização dia 13

--- app/src/main/java/com/example/mockup/MainActivity.java
+++ app/src/main/java/com/example/mockup/MainActivity.java
@@ -4,18 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Runnable {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, 2000);
+
     }
-    public void proximaTela(View view){
+
+    @Override
+    public void run() {
+        startActivity(new Intent(this, TelaPrincipal.class));
+        finish();
+
+
+    }
+
+
+    public void proximaTela(View view) {
 
         Intent intent = new Intent(this, Tela2.class);
         startActivity(intent);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
